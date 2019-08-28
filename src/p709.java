fad615afd5f91800a7fcd694a441a93268e550c0
Matteo Esposito <actumatteo@gmail.com>
2019-08-28 16:13:11 -0400

    problem 709

---
+++ src/p709.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class p709 {
+
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("QWERhello"));
+    }
+
+    public static String toLowerCase(String str){
+
+        String[] word = str.split("");
+
+        for (int i = 0; i<word.length; i++){
+            word[i] = word[i].toLowerCase();
+        }
+
+        return String.join("", word);
+    }
+}
